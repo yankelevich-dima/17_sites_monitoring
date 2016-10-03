c5572f6a4a12efadc526ed5b7e9bd55cafa808c6
Dmitry <yankelevich.dima@gmail.com>
2016-10-04 00:59:41 +0300

    added script + readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 17_sites_monitoring
+
+## Usage:
+
+- Create url list with `\n` separator
+- Run `pip install -r requirements.txt`
+- Run `python check_sites_health.py <path_to_url_list>`
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
+import os
+import whois
+import argparse
+import requests
+import datetime
+from urllib.parse import urlparse
+from dateutil import relativedelta
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as infile:
+        text = infile.read()
+    return filter(None, text.split('\n'))
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return requests.get(url).status_code == 200
+    except requests.exceptions.BaseHTTPError:
+        return False
+
+
+def is_server_payed_for_month(domain_name):
+    delta = relativedelta.relativedelta(
+        get_domain_expiration_date(domain_name),
+        datetime.datetime.now(),
+    )
+    return bool(delta.months or delta.years)
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    date = whois.whois(domain_name).expiration_date
+    if isinstance(date, datetime.datetime):
+        return date
+    elif isinstance(date, list):
+        return date[0]
+
+
+def output_urls_check(urls):
+    for url in urls:
+        print('url: {}\nreturn HTTP OK (200): {}\ndomain payed for month: {}\n'.format(
+            url,
+            is_server_respond_with_200(url),
+            is_server_payed_for_month(urlparse(url).netloc),
+        ))
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help='Input filename')
+
+    args = parser.parse_args()
+
+    if args.filename and os.path.isfile(args.filename):
+        urls = load_urls4check(args.filename)
+        output_urls_check(urls)
